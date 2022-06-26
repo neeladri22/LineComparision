06480a97f89e3071d63ae0d79dc87e8cc0b2dea2
unknown <neeladri22@gmail.com>
2022-06-26 20:45:59 +0530

    Added UC1 program file

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,7 +10,28 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation program");
+            Program.CalculateLength();
+                
         }
+        public static void CalculateLength()
+        {
+
+            Console.WriteLine("Enter the value of x1 :");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y1 :");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y2:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double val1 = Math.Pow((x2 - x1), 2);
+            double val2 = Math.Pow((y2 - y1), 2);
+            double res = Math.Sqrt(val1 + val2);
+            Console.WriteLine("The length of a Line is :" + res);
+
+        }
+
     }
 }
