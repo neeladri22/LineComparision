38d3d2fc6a994f9deaefd05008ca59c9c2eebde7
unknown <neeladri22@gmail.com>
2022-06-26 20:54:40 +0530

    Added UC2 program file

--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,12 +10,11 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Program.CalculateLength();
-                
-        }
-        public static void CalculateLength()
-        {
+            Program.TwoLinesComparison();
 
+        }
+        public static void TwoLinesComparison()
+        {
             Console.WriteLine("Enter the value of x1 :");
             double x1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter the value of y1 :");
@@ -26,12 +25,34 @@ namespace LineComparision
             Console.WriteLine("Enter the value of y2:");
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-            double val1 = Math.Pow((x2 - x1), 2);
-            double val2 = Math.Pow((y2 - y1), 2);
-            double res = Math.Sqrt(val1 + val2);
-            Console.WriteLine("The length of a Line is :" + res);
+            Console.WriteLine("Enter the value of x3 :");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y3 :");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x4:");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y4:");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+            double a = Math.Pow((x2 - x1), 2);
+            double b = Math.Pow((y2 - y1), 2);
+            double result1 = Math.Sqrt(a + b);
+
+            double c = Math.Pow((x4 - x3), 2);
+            double d = Math.Pow((y4 - y3), 2);
+            double result2 = Math.Sqrt(a + b);
+
+            if (result1 == result2)
+            {
+                Console.WriteLine("Line-A and Line-B are equal length");
+            }
+            else
+            {
+                Console.WriteLine("LineA and LineB are not equal length");
+            }
+
 
         }
-
     }
 }
