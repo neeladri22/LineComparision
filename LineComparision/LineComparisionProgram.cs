5114f78f50f1968cf9a9de53a0c04fb83393e11a
unknown <neeladri22@gmail.com>
2022-06-30 11:51:34 +0530

    Added UC4 Program file

---
+++ LineComparision/LineComparisionProgram.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class LineComparisionProgram
+    {
+        public static void lineComparisionCheck()
+        {
+            Console.WriteLine("Enter the Values of First Line");
+
+            Console.WriteLine("Enter First Point");
+            Console.WriteLine("X1=");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y1=");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            Console.WriteLine("Enter Second Point");
+            Console.WriteLine("X2=");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y2=");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            Console.WriteLine("Enter the Values of Second Line");
+
+            Console.WriteLine("Enter First Point");
+            Console.WriteLine("X3=");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y3=");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            Console.WriteLine("Enter Second Point");
+            Console.WriteLine("X4=");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Y4=");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("........................");
+
+            double LenghtofLineOne = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Lenght of First Line is : {0}", LenghtofLineOne);
+            Console.WriteLine("........................");
+            double LenghtofLineTwo = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            Console.WriteLine("Lenght of Second Line is : {0}", LenghtofLineTwo);
+            int Result = LenghtofLineOne.CompareTo(LenghtofLineTwo);
+            if (Result > 0)
+            {
+                Console.WriteLine("First Line is Greater than Second Line");
+            }
+            else if (Result < 0)
+            {
+                Console.WriteLine("Second Line is Greater tahn First Line");
+            }
+            else
+            {
+                Console.WriteLine("First Line and Second Lines are Equal");
+            }
+        }
+    }
+}
--- LineComparision/Program.cs
+++ LineComparision/Program.cs
@@ -10,53 +10,10 @@ namespace LineComparision
     {
         static void Main(string[] args)
         {
-            Program.Comparelines();
+            LineComparisionProgram.lineComparisionCheck();
 
         }
-        public static void Comparelines()
-        {
-            Console.WriteLine("Enter the value of x1 :");
-            double x1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the value of y1 :");
-            double y1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the value of x2:");
-            double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the value of y2:");
-            double y2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the value of x3 :");
-            double x3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the value of y3 :");
-            double y3 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the value of x4:");
-            double x4 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter the value of y4:");
-            double y4 = Convert.ToDouble(Console.ReadLine());
-
-            double a = Math.Pow((x2 - x1), 2);
-            double b = Math.Pow((y2 - y1), 2);
-            double result1 = Math.Sqrt(a + b);
-
-            double c = Math.Pow((x4 - x3), 2);
-            double d = Math.Pow((y4 - y3), 2);
-            double result2 = Math.Sqrt(a + b);
-
-            if (result1 == result2)
-            {
-                Console.WriteLine("Line-A and Line-B are equal length");
-            }
-            else if (result1 > result2)
-            {
-                Console.WriteLine("Length of Line-A is greater than Line-B");
-            }
-            else
-            {
-                Console.WriteLine("Length of Line-B is greater than Line-A");
-            }
-        }
-
+       
 
     }
 }
